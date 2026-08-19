79c1f43ace11ab8cb708a6088523fbdd6b5e5250
Ankith R <ankith.ravishankar@mail.com>
2026-08-19 12:14:39 +0530

    Removed folder

---
+++ #Pascal's Triangle.py
@@ -0,0 +1,9 @@
+#Pascal's Triangle
+n=5
+for i in range(n):
+    print(" "*(n-i),end="")
+    value=1
+    for j in range(i+1):
+        print(value,end=" ")
+        value=value*(i-j)//(j+1)
+    print()
